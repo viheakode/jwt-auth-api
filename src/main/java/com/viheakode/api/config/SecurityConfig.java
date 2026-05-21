ba68ea8b47a2149b7d1e1407cb4c516a476c6e4f
viheakode <viheakode787@gmail.com>
2026-05-21 11:36:38 +0700

    add requestMatchers in SecuirytConfig

--- src/main/java/com/viheakode/api/config/SecurityConfig.java
+++ src/main/java/com/viheakode/api/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/", "/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(exception -> exception
